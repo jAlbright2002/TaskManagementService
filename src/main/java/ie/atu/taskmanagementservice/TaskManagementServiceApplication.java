d346a27f12455fa9ee1ed1dc88488308e7f7108b
james <G00379271@atu.ie>
2024-12-22 04:54:17 +0000

    Project creation

---
+++ src/main/java/ie/atu/taskmanagementservice/TaskManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.taskmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/taskmanagementservice/TaskManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.taskmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
