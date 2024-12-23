60399a8d255643f21e0da0624376064f67463385
james <G00379271@atu.ie>
2024-12-23 03:40:02 +0000

    Added validation for task pojo

--- src/main/java/ie/atu/taskmanagementservice/Tasks/Task.java
+++ src/main/java/ie/atu/taskmanagementservice/Tasks/Task.java
@@ -1,5 +1,8 @@
 package ie.atu.taskmanagementservice.Tasks;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +16,18 @@ public class Task {
     @Id
     private String id;
 
+    @NotBlank(message = "Title is required.")
+    @Size(max = 100, message = "Title cannot be greater than 100 characters.")
     private String title;
 
+    @NotBlank(message = "Status is required.")
+    @Size(max = 50, message = "Status cannot be greater than 50 characters.")
     private String status;
 
+    @Size(max = 500, message = "Description cannot be greater than 500 characters.")
     private String description;
 
+    @Email(message = "Invalid email format.")
     private String email;
 
 }
